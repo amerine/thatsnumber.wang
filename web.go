1ce6c5960c0146d3bd4afca78ed238f74cc58d61
Mark Turner <mark@amerine.net>
2014-10-03 13:05:41 -0700

    play

--- web.go
+++ web.go
@@ -15,6 +15,39 @@ func main() {
 	}
 }
 
+var page = `
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>thats numberwang</title>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-git.js"></script>
+    <style type="text/css">
+      body {
+        overflow: hidden;
+      }
+    </style>
+
+    <script type="text/javascript">//<![CDATA[
+      $(window).load(function(){
+        $(function(){
+          $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+
+          $(window).resize(function(){
+            $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+          });
+        });
+
+      });//]]>
+    </script>
+
+
+  </head>
+  <body>
+    <iframe id="video" src="//www.youtube.com/embed/qjOZtWZ56lc?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+  </body>
+</html>
+`
+
 func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "thats numberwang!")
+	fmt.Fprintln(res, page)
 }
